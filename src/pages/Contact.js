4a7e77cdf1b0113ff151998b67fd34b98699a252
Mehmet Ugur SATIR <musatir@gmail.com>
2022-12-08 12:31:30 +0300

    initial contacts page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+import { IoIosDocument } from "react-icons/io";
+import styled from "styled-components";
+import { Link, Navigate } from "react-router-dom";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const linkedin = "https://www.linkedin.com/in/mehmet-ugur-satir/";
+  const github = "https://github.com/musatirgithub";
+  return (
+    <Wrapper>
+      <div className="main-center">
+        <ul>
+          <li>
+            <a href={linkedin}>
+              <FaLinkedin size="60" color="var(--clr-grey-1)" />
+            </a>
+          </li>
+          <li>
+            <a href={github}>
+              <FaGithubSquare size="60" color="var(--clr-grey-1)" />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:musatir@gmail.com">
+              <MdMail size="60" color="var(--clr-grey-1)" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <IoIosDocument size="60" color="var(--clr-grey-1)" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  .main-center {
+    min-height: calc(100vh - 15rem);
+    margin-top: 5rem;
+  }
+  .main-center > ul {
+    display: flex;
+    justify-content: center;
+    gap: 2.5rem;
+  }
+`;
 
 export default Contact;
